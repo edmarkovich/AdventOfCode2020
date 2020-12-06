1d4f38de3a25785d5edd572f540f0d821f795812
Ed Markovich <em1258@stern.nyu.edu>
2020-12-06 14:29:55 -0500

    :muscle: clean up output

--- Day5/rules.pl
+++ Day5/rules.pl
@@ -10,13 +10,11 @@ seat_row([B|_], _, RangeB, 10, Out) :-
 seat_row([F|T], RangeA, _, 7, Out) :-
 	char_code('F', F),
 	seat_row(T, 0, 7, 8, Out1),
-	write(Out1),
 	Out is (RangeA * 8) + Out1.
 
 seat_row([B|T], _, RangeB, 7, Out) :-
 	char_code('B', B),
 	seat_row(T, 0, 7, 8, Out1),
-	write(Out1),
 	Out is (RangeB * 8) + Out1.
 
 seat_row([F|T], RangeA, RangeB, Position, Out) :-
