4af72ebd960f1b938cc77563bd7df6e827362bfe
Ed Markovich <em1258@stern.nyu.edu>
2020-12-09 09:46:30 -0500

    2015 day 6 part 2 done

---
+++ 2015/Day6/day06b.cpp
@@ -0,0 +1,75 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <cstring>
+#include <sstream>
+using namespace std;
+
+
+enum actionTypes {on, off, toggle};
+typedef pair<int,int> Coords;
+
+int lights[1000][1000];
+
+Coords parseCoords (const string &s) {
+
+	stringstream ss(s);
+	string tmp1, tmp2;
+	getline(ss, tmp1, ','); 
+	getline(ss, tmp2, ',');
+
+	return Coords(stoi(tmp1),stoi(tmp2));
+}
+
+void doIt(actionTypes action, Coords from, Coords to) {
+	
+	
+	for (int i=from.first; i<=to.first; ++i) {
+		for (int j=from.second; j<=to.second; j++) {
+				switch (action) {
+					case on: 	lights[i][j]++; 
+								 break;
+					case off: 	lights[i][j]--;
+								lights[i][j] = max(lights[i][j], 0);
+								break;
+					case toggle: lights[i][j]+=2;
+				}
+		}
+	}
+}
+
+int count() {
+	int sum = 0;
+	for (int i=0; i<1000; ++i) {
+		for (int j=0; j<1000; j++) {
+			sum += lights[i][j];
+		}
+	}
+	return sum;
+}
+
+int main() {
+	ifstream infile("input.txt");
+	
+	string verb;
+	actionTypes action;
+	string coords1, coords2;
+	
+	while(infile >> verb) {
+		if (verb == "turn") {
+			infile >> verb;
+			action = (verb=="on")?on:off;
+		} else {
+			action = toggle;
+		}
+		
+		infile >> coords1;
+		infile >> verb; //consume the "through"
+		infile >> coords2;
+		
+		doIt(action, parseCoords(coords1), parseCoords(coords2));
+	}
+	
+	cout << count() << endl;
+}
