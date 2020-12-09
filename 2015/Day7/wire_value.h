a3fb55a6e6450d53c158806b14bf5f3c7904b8e3
Ed Markovich <em1258@stern.nyu.edu>
2020-12-09 12:39:46 -0500

    2015 day 7 part 1

---
+++ 2015/Day7/wire_value.h
@@ -0,0 +1,88 @@
+#include <unordered_map>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+enum GateType {AND, OR, LSHIFT, RSHIFT, NOT, ASSIGN};
+GateType s2gt(const string &gt) {
+		if (gt == "AND") return AND;
+		else if (gt == "OR")  return OR;
+		else if (gt == "RSHIFT") return RSHIFT;
+		else if (gt == "LSHIFT") return LSHIFT;
+		else cout << "Oper error: " << gt << endl;
+		return AND;
+}
+
+int depth = 0;
+
+
+class WireValue {
+	public:	virtual unsigned short getValue() =0;
+};
+
+unordered_map<string, WireValue*> the_map;
+
+void printSpaces(int x) {
+	for (int i=0; i<x; ++i) {
+		cout << " ";
+	}
+}
+
+
+unordered_map<string, unsigned short> cache;
+
+unsigned short stringToValue(const string &s) {
+		
+	if (cache.find(s) != cache.end()) {
+		return cache[s];
+	} 
+	
+	//printSpaces(depth);
+	depth+=2;
+	//cout << "Looking for " << s << " depth " << depth << endl;
+	unsigned short out;
+	if (isdigit(s[0])) out = stoi(s);
+	else out = the_map[s]->getValue();
+	//printSpaces(depth);
+	depth-=2;
+	//cout << "Got: " << out << endl;
+	cache[s] = out;
+	return out;
+}
+
+
+class IntValue : public WireValue {
+	unsigned short val;
+	
+	public:
+	IntValue(int x) : val(x) {};
+	unsigned short getValue() {
+		//printSpaces(depth);
+		//cout << "Literal: " << val << endl;
+		return val; 
+	};
+};
+
+class GateValue : public WireValue {
+	const string inputA, inputB;
+	GateType gateType;
+	
+	public:
+	GateValue(GateType gt, const string &a, const string &b) : gateType(gt), inputA(a), inputB(b) {}
+
+	unsigned short getValue() {
+		//printSpaces(depth);
+		//cout << "Gate: " << gateType << " " << inputA << " " << inputB << endl;
+		
+		switch(gateType) {
+			case AND:    return stringToValue(inputA) & stringToValue(inputB); 
+			case OR:     return stringToValue(inputA) | stringToValue(inputB);
+			case LSHIFT: return stringToValue(inputA) << stringToValue(inputB);
+			case RSHIFT: return stringToValue(inputA) >> stringToValue(inputB);
+			case NOT:    return ~stringToValue(inputA);
+			case ASSIGN: return stringToValue(inputA);
+		}
+		cout << "Error" << endl;
+		return -1;
+	}
+};
