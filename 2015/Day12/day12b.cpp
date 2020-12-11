a9a5ffe2b6f9b23657a8a50d2b71a75c633773b1
Ed Markovich <em1258@stern.nyu.edu>
2020-12-10 23:17:53 -0500

    2015 day 12 done

--- 2015/Day12/day12b.cpp
+++ 2015/Day12/day12b.cpp
@@ -4,27 +4,101 @@
 #include <fstream>
 using namespace std;
 
-int sumString(const string &str) {
 
-	int sum = 0;
+
+int parse_string(const char **s) {
+	assert(**s=='\"');
+	++(*s);
 	
-	regex r("-?([0-9]+)");
-	smatch m;
+	if ((*s)[0]=='r' && (*s)[1]=='e' && 
+		(*s)[2]=='d' && (*s)[3]=='\"') {
+			(*s)+=4;
+			return 1;
+		}	
+		
+		while(**s!='\"') {
+			++(*s);
+		}
+		++(*s);
+		return 0;
+}
+	
+int parse_number(const char **s) {
+	int sum=0;
+	int sign=1;
+	
+	if (**s=='-') {
+		sign = -1;
+		++(*s);
+	}
+	
+	while(**s>='0' && **s<='9') {
 
-	string::const_iterator ss ( str.cbegin() );
-	while( regex_search (ss, str.cend(), m, r)) {
-		//cout << m[0] << endl;
-		sum += stoi(m[0]);
-		ss = m.suffix().first;
-	}	
-	return sum;
+		sum *= 10;
+		sum += (**s - '0');
+		++(*s);
+	}
+	
+	return sign*sum;
+}
+
+int parse_array(const char**s); //fwd decl.
+int parse_json(const char **s) {
+	int out = 0;
+	int red = 0;
+	
+	assert(**s=='{');
+	++(*s);
+	
+	while(**s!='}') {
+		parse_string(s);
+		assert(**s==':');
+		++(*s);
+
+		switch (**s) {
+			case '[' : out+=parse_array(s);  break;
+			case '{' : out+=parse_json(s);   break;
+			case '\"': red+=parse_string(s); break; 
+			default  : out+=parse_number(s); break;
+		}
+		assert(**s==',' || **s=='}');
+		if (**s=='}') break;
+		++(*s);
+	}
+	++(*s);
+	
+	return (red)?0:out;
+	
+}
+
+
+int parse_array(const char **s) {
+	int out = 0;
+	assert(**s=='[');
+	++(*s);
+	
+	while (**s!= ']') {
+		switch (**s) {
+			case '[' : out+=parse_array(s);  break;
+			case '{' : out+=parse_json(s);   break;
+			case '\"': parse_string(s); break; 
+			default   : out+=parse_number(s); break;
+		}
+		assert(**s==',' || **s==']');
+		if(**s==']') break;
+		++(*s);
+	}
+	++(*s);
+	return out;
 }
 
 int main() {
 	ifstream infile("input.txt");
 	string s;
 	while (infile>>s) {
-		cout << sumString(s) << endl;
+		const char *x = s.c_str();
+		cout << parse_array(&x) << endl;
+		cout << x[0] << endl;
 	}
 	return 0;
 }
